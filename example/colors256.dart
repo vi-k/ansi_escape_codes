8db1ca9ba7fca4a2b4fff07fdc6eacb87f174527
nashol <victor.dunaev@gmail.com>
2026-02-25 12:25:10 +0500

    update example/colors256.dart

--- example/colors256.dart
+++ example/colors256.dart
@@ -1,87 +1,112 @@
 import 'dart:io';
+import 'dart:math' as math;
 
 import 'package:ansi_escape_codes/ansi_escape_codes.dart';
-import 'package:ansi_escape_codes/controls.dart';
 
+String _c(String value, int width) {
+  final diff = width - value.length;
+  if (diff <= 0) return value;
+  return '${' ' * (diff ~/ 2)}$value${' ' * (diff - diff ~/ 2)}';
+}
+
+/// Usage:
+///
+/// ```bash
+/// dart run example/colors256.dart
+/// ```
 void main() {
   stdout
     ..writeln()
-    ..writeln('Standard colors:');
+    ..writeln('256-color table');
 
-  for (var i = 0; i < 16; i++) {
-    final s = ' ${'$i'.padLeft(3, '0')} ';
-
-    if (i % 8 == 0) stdout.writeln();
-
-    stdout
-      ..write(fg256(i % 8 == 7 ? GRAY0 : GRAY23))
-      ..write(fg256Open)
-      ..write(i % 8 == 7 ? GRAY0 : GRAY23)
-      ..write(fg256Close)
-      ..write(bg256(i))
-      ..write(s)
-      ..write(reset)
-      ..write(fg256(i))
-      ..write(s)
-      ..write(reset);
-  }
-  stdout
-    ..writeln()
-    ..writeln()
-    ..writeln('RGB colors:');
-
-  for (var r = 0; r < 6; r++) {
+  {
     stdout
       ..writeln()
-      ..writeln('r=$r')
-      ..write('    ');
-    for (var b = 0; b < 6; b++) {
-      stdout.write(' b=$b      ');
-    }
-    for (var g = 0; g < 6; g++) {
-      stdout.write('\ng=$g ');
+      ..writeln('Standard colors:')
+      ..writeln();
 
-      for (var b = 0; b < 6; b++) {
-        final c = rgb(r, g, b);
-        final s = ' ${'$c'.padLeft(3, '0')} ';
+    final values = List<(Color256, Color256)>.generate(16, (i) {
+      final color = Color256(Colors.values[i]);
+      return (color.withPrefix('fg'), color.withPrefix('bg'));
+    });
 
+    int combine(int max, (Color256, Color256) value) =>
+        math.max(math.max(max, value.$1.id.length), value.$2.id.length);
+
+    final colWidths = [
+      values.take(8).fold(0, combine) + 2,
+      values.skip(8).fold(0, combine) + 2,
+    ];
+
+    for (var i = 0; i < 8; i++) {
+      for (var j = 0; j < 2; j++) {
+        final index = i + j * 8;
+        final (fg, bg) = values[index];
+        if (j != 0) stdout.write('  ');
         stdout
-          ..write(
-            fg256(g >= 3 ? GRAY0 : GRAY23),
-          )
-          ..write(bg256(c))
-          ..write(s)
+          ..write(index.toString().padRight(2))
+          ..write(fg256(fg.index))
+          ..write(_c(fg.id, colWidths[j]))
           ..write(reset)
-          ..write(fg256(c))
-          ..write(s)
+          ..write(bg256(bg.index))
+          ..write(_c(bg.id, colWidths[j]))
           ..write(reset);
       }
+      stdout.writeln();
     }
-    stdout.writeln();
   }
 
-  stdout.writeln('\nGrayscale colors:');
-  for (var i = 0; i < 24; i++) {
-    final c = gray(i);
-    final s = ' ${'$c'.padLeft(3, '0')} ';
+  {
+    stdout
+      ..writeln()
+      ..writeln('RGB colors:')
+      ..writeln();
 
-    if (i % 8 == 0) {
-      stdout.writeln();
-      for (var g = 0; g < 8; g++) {
-        final n = i + g;
-        stdout.write('  ${n.toString().padRight(2)}      ');
+    const columns = 3;
+    const fieldWidth = 13;
+
+    for (var i = 0; i < 216 ~/ columns; i++) {
+      for (var j = 0; j < columns; j++) {
+        final index = 16 + i + j * 216 ~/ columns;
+        final color = Color256(Colors.values[index]);
+        if (j != 0) stdout.write('  ');
+        stdout
+          ..write(index.toString().padRight(3))
+          ..write(fg256(index))
+          ..write(_c(color.withPrefix('fg').id, fieldWidth))
+          ..write(reset)
+          ..write(bg256(index))
+          ..write(_c(color.withPrefix('bg').id, fieldWidth))
+          ..write(reset);
       }
       stdout.writeln();
     }
-
-    stdout
-      ..write(fg256(i >= 16 ? GRAY0 : GRAY23))
-      ..write(bg256(c))
-      ..write(s)
-      ..write(reset)
-      ..write(fg256(c))
-      ..write(s)
-      ..write(reset);
   }
-  stdout.writeln();
+
+  {
+    stdout
+      ..writeln()
+      ..writeln('Grayscale colors:')
+      ..writeln();
+
+    const columns = 3;
+    const fieldWidth = 13;
+
+    for (var i = 0; i < 24 ~/ columns; i++) {
+      for (var j = 0; j < columns; j++) {
+        final index = 232 + i + j * 24 ~/ columns;
+        final color = Color256(Colors.values[index]);
+        if (j != 0) stdout.write('  ');
+        stdout
+          ..write(index.toString().padRight(3))
+          ..write(fg256(index))
+          ..write(_c(color.withPrefix('fg').id, fieldWidth))
+          ..write(reset)
+          ..write(bg256(index))
+          ..write(_c(color.withPrefix('bg').id, fieldWidth))
+          ..write(reset);
+      }
+      stdout.writeln();
+    }
+  }
 }
